e5a4c97b4ce2f07f50f8549f968da40c9adc6413
Samuel Wallace Stegall <stegall@tamu.edu>
2020-10-04 19:47:47 -0500

    Cleaned up the homepage.
    Started implementing the query page.

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notflix.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index(string genre)
+        {
+            ViewData["genre"] = genre;
+            return View();
+        }
+    }
+}
